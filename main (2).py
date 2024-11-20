fb24dd6ecc8f29b69b1b94419c8249d49dfaf317
Елизавета <magomedovaliza813@gmail.com>
2024-11-20 23:12:37 +0200

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,21 @@
+import math 
+
+def calcular_mass(area, density, velocity )
+S = 3,14^2/4
+g = 9,81
+V = velocity
+
+c = 0.75
+rho = 1.225 
+k = 0.6 * (rho / density) ** 0.5 
+
+mass = ((2,4649 * 0.75 * 1.225 * v ** 2)) / (2 * g)) + (k * S)
+
+return mass  
+
+area = float(input("2,4649 (м2): ))
+density = float(input("1.225 (кг/м3): "))
+veloocity = float((input("5-10 (м/с): ")))
+
+result = calcular_mass(area, density, velocity)
+print("Масса тела составляет: ", 4,7 кг. ")
